e94661154455efbefbcb1d19100842e1d50c67f8
wusongyuan <wusongyuan@yiche.com>
2020-03-06 19:16:23 +0800

    添加springcloud config模块

---
+++ springcloud-config/src/main/java/com/qqxhb/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.qqxhb.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+	
+}
